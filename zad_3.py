ff5713b4f35a37418178ef8eb9f20cc23719b411
Rafał Kwiatek <rafal.kwiatek@edu.uekat.pl>
2022-10-10 14:46:52 +0200

    nowy user

---
+++ zad_3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 10 14:35:31 2022
+
+@author: student
+"""
+
+#1
+
+def zad1(name , surname):
+    return "czesc "+name + " " + surname
+
+print(zad1("Jan","Kowalski"))
